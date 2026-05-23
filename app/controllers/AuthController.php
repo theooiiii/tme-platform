b51d5ea1450e41db5f32cea8b61e230b8993d99c
Nilo Theodoro <niloantonio09@gmail.com>
2026-05-23 18:46:20 -0300

    feat(auto-sync): atualizar camadas MVC
    Arquivos sincronizados automaticamente:
    - app/controllers/AuthController.php

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -5,10 +5,12 @@ defined('BASE_PATH') || exit('Acesso direto nao permitido.');
 class AuthController extends Controller
 {
     private User $users;
+    private GamificationService $gamification;
 
     public function __construct()
     {
         $this->users = new User();
+        $this->gamification = new GamificationService();
     }
 
     public function showLogin(): void
@@ -88,6 +90,7 @@ class AuthController extends Controller
         session_regenerate_id(true);
         $_SESSION['user_id'] = (int) $user['id'];
         $this->users->markLastLogin((int) $user['id']);
+        $this->gamification->firstLogin((int) $user['id']);
 
         $this->redirect('/portal');
     }
